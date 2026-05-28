2a426dec7cef4ba08f74e18c1f448758b67662b9
Illia Davydiuk <92821486+illiadavydiuk@users.noreply.github.com>
2026-05-28 15:42:50 +0300

    feat: add DiggingDeeperController with collections method and routing

--- web-server/blog/routes/web.php
+++ web-server/blog/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +18,12 @@ Route::middleware([
     })->name('dashboard');
 });
 Route::apiResource('rest', RestTestController::class)->names('restTest');
+
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
+});
